68b503a06014dc0c7616bcf9107a2ae7a0cb3498
Aman Singh <151167011+amansingh7070@users.noreply.github.com>
2024-10-05 00:26:02 +0530

    local to github

---
+++ src/Constants/Constant.js
@@ -0,0 +1,6 @@
+export const logo = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Ffood-logo&psig=AOvVaw2Sn843VpaS8Lqs09vioWp6&ust=1711633183212000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLiFvNPIlIUDFQAAAAAdAAAAABAS';
+
+
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
